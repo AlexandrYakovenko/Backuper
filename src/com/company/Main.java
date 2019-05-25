23e5d7ebf6763670a6b376142ecfba51fb5eb0da
Alexandr Yakovenko <yakovenko9814@ukr.net>
2019-05-25 19:22:26 +0300

    Changed path to backup

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Backuper bp = new Backuper("C:\\Users\\Professional\\Pictures", "C:\\Users\\Professional\\Documents", 5000);
+        Backuper bp = new Backuper("C:\\Users\\Александр\\Pictures\\Sourse", "C:\\Users\\Александр\\Pictures\\Destination", 5000);
         bp.run();
     }
 }
